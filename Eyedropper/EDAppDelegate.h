c55792061c30e1c5c9ab3cc450df19129468eb45
twistedTightly <mrauh1@nd.edu>
2013-10-31 23:14:49 +0800

    Initial Commit

---
+++ Eyedropper/EDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EDAppDelegate.h
+//  Eyedropper
+//
+//  Created by Maribeth Rauh on 10/31/13.
+//  Copyright (c) 2013 mrauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Eyedropper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Eyedropper
+//
+//  Created by Maribeth Rauh on 10/31/13.
+//  Copyright (c) 2013 mrauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EDAppDelegate class]));
+    }
+}
