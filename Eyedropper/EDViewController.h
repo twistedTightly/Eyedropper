2e400034c9f63f18d4e16b1b5f3f45f7dd1fb765
twistedTightly <mrauh1@nd.edu>
2013-11-04 01:47:40 +0800

    Main vc set as image picker delegate
    Much code (especially for delegate methods) taken from docs

--- Eyedropper/EDViewController.h
+++ Eyedropper/EDViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface EDViewController : UIViewController
+@interface EDViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @end
--- Eyedropper/EDViewController.m
+++ Eyedropper/EDViewController.m
@@ -33,12 +33,86 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    [self startImagePickerFromViewController:self WithDelegate:self];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (BOOL) startImagePickerFromViewController: (UIViewController *) controller WithDelegate:
+        (id <UINavigationControllerDelegate, UIImagePickerControllerDelegate>) delegate
+{
+    // First check if the device has a camera that is available
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]
+        || delegate == nil)
+    {
+        NSLog(@"No image picker for you");
+        return NO;
+    }
+    
+    /* Instantiate and set up the image picker */
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = delegate;
+    return YES;
+}
+
+
+#pragma mark UIImagePickerControllerDelegate Methods
+
+// For responding to the user tapping Cancel
+- (void) imagePickerControllerDidCancel: (UIImagePickerController *) picker
+{
+    [[picker parentViewController] dismissViewControllerAnimated:YES completion:nil];
+    //[[picker parentViewController] dismissModalViewControllerAnimated: YES]; // iOS 5 and lower
+    //[picker release]; // pre-ARC - so is this handled now?
+}
+
+// For responding to the user accepting a newly-captured picture or movie
+- (void) imagePickerController: (UIImagePickerController *) picker
+ didFinishPickingMediaWithInfo: (NSDictionary *) info
+{
+    // Will indicate if it is a movie and a still image by its UTI (kUTTypeImage or kUTTypeMovie)
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    UIImage *originalImage;
+    
+    // Handle a still image capture by checking the media type
+    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeImage, 0)
+        == kCFCompareEqualTo)
+    {
+        // Not dealing with edited images yet
+//        editedImage = (UIImage *) [info objectForKey:
+//                                   UIImagePickerControllerEditedImage];
+        originalImage = (UIImage *) [info objectForKey:
+                                     UIImagePickerControllerOriginalImage];
+        
+//        if (editedImage) {
+//            imageToSave = editedImage;
+//        } else {
+//            imageToSave = originalImage;
+//        }
+        
+        // Save the new image (original or edited) to the Camera Roll
+        UIImageWriteToSavedPhotosAlbum (originalImage, nil, nil , nil);
+    }
+    
+//    // Handle a movie capture
+//    if (CFStringCompare ((CFStringRef) mediaType, kUTTypeMovie, 0)
+//        == kCFCompareEqualTo) {
+//        
+//        NSString *moviePath = [[info objectForKey:
+//                                UIImagePickerControllerMediaURL] path];
+//        
+//        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum (moviePath)) {
+//            UISaveVideoAtPathToSavedPhotosAlbum (
+//                                                 moviePath, nil, nil, nil);
+//        }
+//    }
+//    
+    [[picker parentViewController] dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
