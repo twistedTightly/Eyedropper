346a4f1e21fcf1e61a472dde420892f22e7f28ba
twistedTightly <mrauh1@nd.edu>
2013-12-04 22:14:33 +0800

    Fixed storyboard not loading problem

--- Eyedropper/EDAppDelegate.m
+++ Eyedropper/EDAppDelegate.m
@@ -13,14 +13,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    // Set root view controller
-    EDViewController *mainVC = [[EDViewController alloc] init];
-    [self.window setRootViewController:mainVC];
-    
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
